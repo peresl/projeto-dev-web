b1d3397ce3a2f6e26d83c28b1aed4c47becb5f38
Jardson <136630558+peresl@users.noreply.github.com>
2024-09-18 08:29:00 -0300

    Delete Firewall_VF directory

--- Firewall_VF/js/firewallMain.js
+++
@@ -1,8 +0,0 @@
-import { updateChains, addRule, clearRules } from './firewallFunctions.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    updateChains();
-    document.getElementById('table').addEventListener('change', updateChains);
-    document.getElementById('addRuleBtn').addEventListener('click', addRule);
-    document.getElementById('clearRulesBtn').addEventListener('click', clearRules);
-});
