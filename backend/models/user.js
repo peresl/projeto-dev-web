2f956a257f4813f7b4d49d8410110887ccd0c6eb
Jardson <136630558+peresl@users.noreply.github.com>
2024-09-30 07:30:50 -0300

    Delete backend directory

--- backend/models/user.js
+++
@@ -1,9 +0,0 @@
-// Exemplo simples de modelo de usuário
-class User {
-    constructor(username, password) {
-        this.username = username;
-        this.password = password; // Senha deve ser hasheada em produção
-    }
-}
-
-module.exports = { User };
